7056be970bdc4c669473e3f13ed2dff1237bb066
Allen C <dadarksideofdamoon@yahoo.com>
2020-09-07 13:01:12 -0400

    Updated added react-router to hit menu-item to there menue pages

--- src/components/menu-item/menu-item.componet.jsx
+++ src/components/menu-item/menu-item.componet.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 import './menu-iteams.styles.scss';
 
-const MenuIteam = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
+const MenuIteam = ({ title, imageUrl, size, history, linkUrl, match }) => (
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className='background-image' style={{
             backgroundImage: `url(${imageUrl})`
         }} />
@@ -16,4 +17,4 @@ const MenuIteam = ({ title, imageUrl, size }) => (
 
 );
 
-export default MenuIteam;
+export default withRouter(MenuIteam);
